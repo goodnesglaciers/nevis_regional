90fb6bb7624f29d1c8672e1f78aae9779be57376
Laura A. Stevens <37626306+goodnesglaciers@users.noreply.github.com>
2018-06-22 18:17:36 -0400

    Delete runoff2.m

--- nevis/runoff2.m
+++
@@ -1,7 +0,0 @@
-function [r] = runoff(day,runoff_2011_nevis)
-% 10 May 2016 LAS
-% load RACMO daily runoff values over 160 km region
-day = round(day);                    % round time to nearest interger day
-% reshape to gg.nIJ (distributed input at nodes)
-r = ((runoff_2011_nevis(day,:))./(1000*86400))'; % runoff on year and day of interest (m/sec)
-end
